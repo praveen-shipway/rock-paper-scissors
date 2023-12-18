23a5c3cd286d34f8bf1830df0f0f1e715ff84018
praveen <praveen.mangal@shipway.com>
2023-12-18 22:04:52 +0530

    write logic of game

--- script.js
+++ script.js
@@ -2,3 +2,46 @@ function getComputerChoice() {
 	const $values = ['rock', 'paper', 'scissors'];
 	return $values[Math.floor(Math.random() * $values.length)];
 }
+
+function playRound(playerSelection, computerSelection) {
+
+	if (playerSelection.toLowerCase() === computerSelection.toLowerCase() ) {
+		return "Ties";
+	}
+
+	if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'paper' ) {
+		return "You Lose! Paper beats Rock";
+	}
+
+	if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'scissors' ) {
+		return "You Lose! scissors beats Paper";
+	}
+
+	if (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'rock' ) {
+		return "You Lose! Rock beats Scissors";
+	}
+
+	return "You Win!";
+}
+
+function game() {
+	let score = 0;
+
+	for (let i = 0; i < 5; i++) {
+		let playerSelection = prompt("Enter 'Rock', 'Paper', 'Scissors'", "");
+		let computerSelection = getComputerChoice();
+
+		let result = playRound(playerSelection, computerSelection);
+		console.log(result);
+		
+		if (result.includes('Ties')) {
+			i--;
+		} else if (!result.includes('Lose')) {
+			score++;
+		}
+	}
+	return score;
+}
+
+let finalScore = game();
+console.log("Final score is " + finalScore);
